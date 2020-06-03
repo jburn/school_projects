6804731a8db063d491c1691791f5ff71db585d8e
Juho B <jopbruun@gmail.com>
2020-06-03 19:09:59 +0300

    fixed some minor problems

--- pocket_calculator/calculator.py
+++ pocket_calculator/calculator.py
@@ -29,34 +29,35 @@ used by the '=' button
 """
 
 def calculate():
-    try:
-        if states["operand"] == "plus":
-            answer = op.add(float(states["numbers"][0]), float(states["numbers"][1]))
-        elif states["operand"] == "minus":
-            answer = op.sub(float(states["numbers"][0]), float(states["numbers"][1]))
-        elif states["operand"] == "multiply":
-            answer = op.mul(float(states["numbers"][0]), float(states["numbers"][1]))
-        elif states["operand"] == "divide":
-            answer = op.truediv(float(states["numbers"][0]), float(states["numbers"][1]))
-        elif states["operand"] == "power":
-            answer = op.pow(float(states["numbers"][0]), float(states["numbers"][1]))
-        states["hiddenline"] = str(answer)
-        calcline.set(str(answer))
-        states["moves"] = 0
-        states["finished"] = 1
-        states["numbers"][0] = answer
-        states["numbers"][1] = ""
-        states["dot"] = 0
-    except ValueError:
-        pass
-       # print("Error: Add another number to the operation")
-    except UnboundLocalError:
-        pass
-        #print("Error: Cannot operate on a single number")
-    except OverflowError:
-       # print("Error: Result too large")
-        calcline.set("Result too large, press clear")
-        states["finished"] = 2
+    if states["finished"] != 2:    
+        try:
+            if states["operand"] == "plus":
+                answer = op.add(float(states["numbers"][0]), float(states["numbers"][1]))
+            elif states["operand"] == "minus":
+                answer = op.sub(float(states["numbers"][0]), float(states["numbers"][1]))
+            elif states["operand"] == "multiply":
+                answer = op.mul(float(states["numbers"][0]), float(states["numbers"][1]))
+            elif states["operand"] == "divide":
+                answer = op.truediv(float(states["numbers"][0]), float(states["numbers"][1]))
+            elif states["operand"] == "power":
+                answer = op.pow(float(states["numbers"][0]), float(states["numbers"][1]))
+            states["hiddenline"] = str(answer)
+            calcline.set(str(answer))
+            states["moves"] = 0
+            states["finished"] = 1
+            states["numbers"][0] = answer
+            states["numbers"][1] = ""
+            states["dot"] = 0
+        except ValueError:
+            pass
+            #print("Error: Add another number to the operation")
+        except UnboundLocalError:
+            pass
+            #print("Error: Cannot operate on a single number")
+        except OverflowError:
+            #print("Error: Result too large")
+            calcline.set("Result too large, press clear")
+            states["finished"] = 2
 
 """
 this function clears the the visible text on the screen and resets all the states to default.
@@ -205,7 +206,7 @@ def addminus():
         states["hiddenline"] += "-"
         calcline.set(states["hiddenline"])
         states["numbers"][0] += "-"
-    elif states["moves"] != 1 and states["hiddenline"] != " " and states["finished"] < 1 and states["hiddenline"] != " -":
+    elif states["moves"] != 1 and states["hiddenline"] != " " and states["finished"] != 2 and states["hiddenline"] != " -":
         states["finished"] -= 1
         states["moves"] = 1
         states["hiddenline"] += "-"
@@ -218,7 +219,7 @@ def addminus():
         calcline.set(states["hiddenline"])
 
 def adddivide():
-    if states["moves"] != 1 and states["finished"] < 1:
+    if states["moves"] != 1 and states["finished"] != 2:
         states["finished"] -= 1
         states["moves"] = 1
         states["hiddenline"] += "/" 
@@ -231,7 +232,7 @@ def adddivide():
         calcline.set(states["hiddenline"])
 
 def addmultiply():
-    if states["moves"] != 1 and states["finished"] < 1:
+    if states["moves"] != 1 and states["finished"] != 2:
         states["finished"] -= 1
         states["moves"] = 1
         states["hiddenline"] += "*"
@@ -244,7 +245,7 @@ def addmultiply():
         calcline.set(states["hiddenline"])
 
 def addpower():
-    if states["moves"] != 1 and states["finished"] < 1:
+    if states["moves"] != 1 and states["finished"] != 2:
         states["finished"] -= 1
         states["moves"] = 1
         states["hiddenline"] += "^"
@@ -258,7 +259,7 @@ def addpower():
 
 def addsquared():
     states["finished"] -= 1
-    if states["moves"] != 1 and states["finished"] < 1:
+    if states["moves"] != 1 and states["finished"] != 2:
         try:
             squared = float(states["hiddenline"]) ** 2
             states["hiddenline"] = str(squared)
@@ -276,12 +277,16 @@ def addsquared():
             print("Error: Cant square without a variable")
 
 def addpoint():
-    if states["finished"] < 1 and states["dot"] != 1:
+    if states["finished"] < 1 and states["dot"] < 1:
         states["numbers"][states["moves"]] += "."
         states["hiddenline"] += "."
         calcline.set(states["hiddenline"])
         states["dot"] = 1
 
+
+"""
+Configuring the interface, grid and different elements of the interface
+"""
 root = Tk()
 root.title("Calculator")
 calcline = StringVar()
@@ -293,32 +298,28 @@ root.rowconfigure(0, weight=1)
 
 ttk.Label(mainframe, textvariable=calcline, font = ('Lucida Console', 15)).grid(column=1, row=1 , sticky=N, columnspan=5)
 
-b1 = ttk.Button(mainframe, text="1", command=addone).grid(column=1, row=2, sticky=(N, W, S, E))
-b2 = ttk.Button(mainframe, text="2", command=addtwo).grid(column=2, row=2, sticky=(N, W, S, E))
-b3 = ttk.Button(mainframe, text="3", command=addthree).grid(column=3, row=2, sticky=(N, W, S, E))
-bplus = ttk.Button(mainframe, text="+", command=addplus).grid(column=4, row=2, sticky=(N, W, S, E))
-bminus = ttk.Button(mainframe, text="-", command=addminus).grid(column=5, row=2, sticky=(N, W, S, E))
+ttk.Button(mainframe, text="1", command=addone).grid(column=1, row=2, sticky=(N, W, S, E))
+ttk.Button(mainframe, text="2", command=addtwo).grid(column=2, row=2, sticky=(N, W, S, E))
+ttk.Button(mainframe, text="3", command=addthree).grid(column=3, row=2, sticky=(N, W, S, E))
+ttk.Button(mainframe, text="+", command=addplus).grid(column=4, row=2, sticky=(N, W, S, E))
+ttk.Button(mainframe, text="-", command=addminus).grid(column=5, row=2, sticky=(N, W, S, E))
 
-b4 = ttk.Button(mainframe, text="4", command=addfour).grid(column=1, row=3, sticky=(N, W, S, E))
-b5 = ttk.Button(mainframe, text="5", command=addfive).grid(column=2, row=3, sticky=(N, W, S, E))
-b6 = ttk.Button(mainframe, text="6", command=addsix).grid(column=3, row=3, sticky=(N, W, S, E))
-bmultiply = ttk.Button(mainframe, text="*", command=addmultiply).grid(column=4, row=3, sticky=(N, W, S, E))
-bdivide = ttk.Button(mainframe, text="/", command=adddivide).grid(column=5, row=3, sticky=(N, W, S, E))
+ttk.Button(mainframe, text="4", command=addfour).grid(column=1, row=3, sticky=(N, W, S, E))
+ttk.Button(mainframe, text="5", command=addfive).grid(column=2, row=3, sticky=(N, W, S, E))
+ttk.Button(mainframe, text="6", command=addsix).grid(column=3, row=3, sticky=(N, W, S, E))
+ttk.Button(mainframe, text="*", command=addmultiply).grid(column=4, row=3, sticky=(N, W, S, E))
+ttk.Button(mainframe, text="/", command=adddivide).grid(column=5, row=3, sticky=(N, W, S, E))
 
-b7 = ttk.Button(mainframe, text="7", command=addseven).grid(column=1, row=4, sticky=(N, W, S, E))
-b8 = ttk.Button(mainframe, text="8", command=addeight).grid(column=2, row=4, sticky=(N, W, S, E))
-b9 = ttk.Button(mainframe, text="9", command=addnine).grid(column=3, row=4, sticky=(N, W, S, E))
-bpower = ttk.Button(mainframe, text="^", command=addpower).grid(column=4, row=4, sticky=(N, W, S, E))
-bsquared = ttk.Button(mainframe, text="x^2", command=addsquared).grid(column=5, row=4, sticky=(N, W, S, E))
+ttk.Button(mainframe, text="7", command=addseven).grid(column=1, row=4, sticky=(N, W, S, E))
+ttk.Button(mainframe, text="8", command=addeight).grid(column=2, row=4, sticky=(N, W, S, E))
+ttk.Button(mainframe, text="9", command=addnine).grid(column=3, row=4, sticky=(N, W, S, E))
+ttk.Button(mainframe, text="^", command=addpower).grid(column=4, row=4, sticky=(N, W, S, E))
+ttk.Button(mainframe, text="x^2", command=addsquared).grid(column=5, row=4, sticky=(N, W, S, E))
 
-bpoint = ttk.Button(mainframe, text=".", command=addpoint).grid(column=1, row=5, sticky=(N, W, S, E))
-b0 = ttk.Button(mainframe, text="0", command=addzero).grid(column=2, row=5, sticky=(N, W, S, E))
-bequals = ttk.Button(mainframe, text="=", command=calculate).grid(column=3, row=5, sticky=(N, W, S, E))
-bclear = ttk.Button(mainframe, text="clear", command=clear).grid(column=5, row=5, sticky=(N, W, S, E))
-
-def errordisable():
-    b1.configure(state=DISABLED)
-    bplus.configure(state=DISABLED)
+ttk.Button(mainframe, text=".", command=addpoint).grid(column=1, row=5, sticky=(N, W, S, E))
+ttk.Button(mainframe, text="0", command=addzero).grid(column=2, row=5, sticky=(N, W, S, E))
+ttk.Button(mainframe, text="=", command=calculate).grid(column=3, row=5, sticky=(N, W, S, E))
+ttk.Button(mainframe, text="clear", command=clear).grid(column=5, row=5, sticky=(N, W, S, E))
 
 """scrapped keyboard support"""
 #root.bind('<Return>', calculate)
